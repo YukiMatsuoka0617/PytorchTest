854773a49d4bc0fd01787037b1d82239d3f1cab2
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2020-09-18 01:44:24 +0900

    Data Loader Code

---
+++ data_loader.py
@@ -0,0 +1,21 @@
+import torch
+from torchvision import datasets, transforms
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+
+def loader():
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=64, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=1000, shuffle=True)
+    return train_loader,test_loader
