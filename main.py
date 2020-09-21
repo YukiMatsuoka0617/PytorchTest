d94c1d52a783826d2d852a55133b0e7627db329e
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2020-09-22 00:22:49 +0900

    add save model

--- main.py
+++ main.py
@@ -10,15 +10,22 @@ import train
 import test
 
 def main():
-    device = torch.device("cpu")    # use the CPU for this
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
     train_loader, test_loader = data_loader.loader()
 
     model = net.Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)    # set up optimizer
-    epochs = 1    # compute epochs
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    epochs = 1    
     for epoch in range(1, epochs + 1):
         train.train(model, device, train_loader, optimizer, epoch)
-        test.test(model, device, test_loader)
+
+    PATH = './mnist_result.pth'
+    torch.save(model.state_dict(), PATH)
+
+    model = net.Net().to(device)
+    model.load_state_dict(torch.load(PATH))
+    test.test(model, device, test_loader)
     
 
 if __name__ == '__main__':
